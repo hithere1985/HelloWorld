4876421ee582367d84bfdca5b591711ae3ac78be
Teamin Jung <realone85@gmail.com>
2021-09-05 03:39:46 +0900

    버튼 클릭해서 이미지 보여주는 기능 추가

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,15 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void onClick(View view) {
-        Toast.makeText(this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_LONG).show();
+        binding.imageView.setImageResource(R.drawable.paris);
+        Toast.makeText(this, "이미지를 보여줍니다", Toast.LENGTH_LONG).show();
     }
 }
